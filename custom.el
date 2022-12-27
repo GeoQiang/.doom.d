ef8b36a0963031c80c6f17fe877151a19e27a6b3
GeoZhao <zq@yasuoo.org>
2022-12-27 17:28:28 +0800

    first commit

---
+++ custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/geo/Dropbox/Todo/remind.org" "/home/geo/Dropbox/Todo/project.org"))
+ '(package-selected-packages '(ox-hugo org-pomodoro writeroom-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:family "Fira Code Retina" :height 110))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.2 :foreground "#black"))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.2 :foreground "#black"))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2 :foreground "#black"))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.2 :foreground "#black"))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.2 :foreground "#black"))))
+ '(org-level-6 ((t (:inherit outline-6 :height 1.2 :foreground "#black"))))
+ '(org-level-7 ((t (:inherit outline-7 :height 1.2 :foreground "#black"))))
+ '(org-level-8 ((t (:inherit outline-8 :height 1.2 :foreground "#black"))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(variable-pitch ((t (:family "ETBembo" :height 180)))))
+(put 'customize-face 'disabled nil)
+(put 'customize-set-value 'disabled nil)
