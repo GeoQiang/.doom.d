ebd4e62515aefb74d73a8251d9a2dabc26aa3160
GeoZhao <zq@yasuoo.org>
2022-12-31 11:08:39 +0800

    change configuration

--- lisp/init-neotree.el
+++
@@ -1,8 +0,0 @@
-;; neotree conf
-(setq projectile-switch-project-action 'neotree-projectile-action)
-(setq neo-show-hidden-files 'nil)
-(setq neo-smart-open 't)
-(global-set-key [f8] 'neotree-toggle)
-
-
-(provide 'init-neotree)
--- packages.el
+++ packages.el
@@ -6,6 +6,7 @@
 ;; use 'M-x doom/reload'.
 
 (package! writeroom-mode)
+(package! org-pomodoro)
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
